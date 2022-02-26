f025ca327a34be622eafc7603dc73e06fbfa0f55
Samito19 <samiamsaf1902@gmail.com>
2022-02-25 20:09:44 -0500

    Update on Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,55 @@
-import React from 'react'
-import './styles/home.css'
+import React from "react";
+import "./styles/home.css";
 
 const Home = () => {
-  return (
-    <div className="home-page__header-container">
+  const renderHeader = () => {
+    return (
+      <div className="home-page__header-container">
         <div className="home-page__logo">
-            <h1>The Drew Marketplace</h1>
+          <h1>The Drew Marketplace</h1>
         </div>
         <div className="home-page__header-options">
-            <p>Buy</p>
-            <p>Sell</p>
-            <p>Trade</p>
-            <p>Login</p>
+          <p>Buy</p>
+          <p>Sell</p>
+          <p>Trade</p>
+          <button className="home-page_header-options-button">Log in</button>
         </div>
-    </div>
-  )
-}
+      </div>
+    );
+  };
 
-export default Home
+  const renderHomePageBody = () => {
+    return (
+      <div className="home-page__body-container">
+        <div className="home-page__body-categories-container">
+            <div className="home-page__body-categories-title">
+            <h2 >Categories</h2>
+            </div>
+          
+          <ul className="home-page__body-categories-choices">
+              <li>
+                  <span>Electronics</span>
+              </li>
+              <li>
+                  <span>Clothing</span>
+              </li>
+              <li>
+                  <span>Gaming</span>
+              </li>
+              <li>
+                  <span>Furniture</span>
+              </li>
+          </ul>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <>
+      {renderHeader()}
+      {renderHomePageBody()}
+    </>
+  );
+};
+
+export default Home;
